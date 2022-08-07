f601b7f3e11b50f2d3bfc4b9e9453eaa911f5ec2
LaScheggia <lascheggia605@gmail.com>
2022-08-07 11:55:06 +0200

    Problem 1 solved

---
+++ Problem 2/index.js
@@ -0,0 +1,28 @@
+var colors = [
+  "E91E63",
+  "2196F3",
+  "4CAF50",
+  "FFEB3B",
+  "673AB7",
+  "FF5722",
+  "4CAF50",
+  "E91E63",
+  "2196F3",
+  "673AB7",
+  "FF5722",
+  "FFEB3B",
+];
+
+function startGame() {
+  var randomColors = shuffle(colors);
+
+  
+
+  
+
+  
+}
+
+
+
+
