17ba83105095c51d3753229cfe5fdba954b431d4
LaScheggia <lascheggia605@gmail.com>
2022-08-08 14:55:47 +0200

    Problem 2 solved

--- Problem 2/index.js
+++ Problem 2/index.js
@@ -1,28 +1,63 @@
-var colors = [
-  "E91E63",
-  "2196F3",
-  "4CAF50",
-  "FFEB3B",
-  "673AB7",
-  "FF5722",
-  "4CAF50",
-  "E91E63",
-  "2196F3",
-  "673AB7",
-  "FF5722",
-  "FFEB3B",
-];
+const cards = document.querySelectorAll('.memory-card');
 
-function startGame() {
-  var randomColors = shuffle(colors);
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
 
-  
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-  
+  this.classList.add('flip');
 
-  
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
+  }
+
+  // second click
+  secondCard = this;
+
+  checkForMatch();
 }
 
+function checkForMatch() {
+  let isMatch = firstCard.dataset.color === secondCard.dataset.color;
 
+  isMatch ? disableCards() : unflipCards();
+}
 
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
